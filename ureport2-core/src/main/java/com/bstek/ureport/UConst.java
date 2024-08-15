973f1716094bdc22b82f82e285a99440eb082559
jiangtao <jiangtao.cctv@qq.com>
2024-08-15 10:52:51 +0800

    chore: 调整目录

--- ureport2-core/src/main/java/com/bstek/ureport/UConst.java
+++
@@ -1,7 +0,0 @@
-package com.bstek.ureport;
-
-public class UConst {
-
-    // 如果这个配置存在，标识接入了数据源
-    public static final String DATASOURCE_EXIST_KEY = "spring.datasource.url";
-}
--- ureport2-core/src/test/java/cn/BigDecimalTest.java
+++
@@ -1,9 +0,0 @@
-package cn;
-
-import java.math.BigDecimal;
-
-public class BigDecimalTest {
-    public static void main(String[] args) {
-        System.out.println(BigDecimal.valueOf(100).divide(BigDecimal.valueOf(0)));
-    }
-}
