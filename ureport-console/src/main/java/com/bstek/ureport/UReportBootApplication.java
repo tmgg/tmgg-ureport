352610b612f76d01ebc9bfb56b1c94a7747a7240
jiangtao <jiangtao.cctv@qq.com>
2024-09-06 08:58:26 +0800

    Content-Security-Policy

--- ureport-console/src/main/java/com/bstek/ureport/UReportBootApplication.java
+++ ureport-console/src/main/java/com/bstek/ureport/UReportBootApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UReportBootApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         SpringApplication.run(UReportBootApplication.class, args);
     }
 }
